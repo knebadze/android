5a3031c8b838e3224174efa905253b4eea587820
knebadze <nebo.nebadze98@gmail.com>
2022-09-20 12:57:47 +0400

    first commit

--- resources/views/admin/users/role.blade.php
+++ resources/views/admin/users/role.blade.php
@@ -19,61 +19,14 @@
 
          <!-- Main content -->
 <section class="content">
-        <div class="container-fluid">
-            <div class="row">
+    <div class="container-fluid">
+        <div class="row">
             <!-- left column -->
             <div class="col-md-12">
                 <div>იუზერის სახელი: {{$user->name}}</div>
                 <div>იუზერის ემაილ: {{$user->email}}</div>
               <!-- /.card -->
             </div><!--/col-md-12 -->
-            
-            <!-- right column -->
-            <div class="col-md-12">
-                @if (Session::has('message'))
-                <div class="alert alert-success" role="alert">
-                    {{Session::get('message')}}!
-                  </div>
-                @endif
-                <!-- jquery validation -->
-                <div class="card card-primary">
-                  <div class="card-header">
-                    <h3 class="card-title">ნებართვის მინიჭება</h3>
-                  </div>
-                  <div class="mt-4 ml-3 flex flex-d">
-                    @if ($role->permissions)
-                        @foreach ($role->permissions as $role_permission)
-                            <form class="ml-1" method="POST" action="{{route('admin.roles.permissions.revoke', [$role->id, $role_permission->id])}}"
-                                onsubmit="return confirm('ნამდვილად გსურთ წაშლა');">
-                                @csrf
-                                @method('DELETE')
-                                <button class="p-2 bg-info text-white ml-2 rounded" type="submit"><i class="nav-icon fas fa-trash"></i> {{$role_permission->name}}</button>
-                            </form>
-                        @endforeach
-
-                    @endif
-                </div><!-- /.card-header -->
-
-                    <!-- form start -->
-                    <form id="quickForm" method="POST" action="{{route('admin.roles.permissions',$role)}}">
-                          @csrf
-                    <div class="card-body">
-                      <div class="form-group">
-                        <label for="name">ნებართვა</label>
-                        <select class="form-control" name="permission" id="">
-                            <option value="">აირჩიე ნებართვა</option>
-                            @foreach ($permissions as $permission)
-                            <option value="{{$permission->name}}">{{$permission->name}}</option>
-                            @endforeach
-                        </select>
-                      </div>
-                    </div><!-- /.card-body -->
-                    <div class="card-footer">
-                      <button type="submit" class="btn btn-primary">შენახვა</button>
-                    </div>
-                    </form>
-                </div><!-- /.card -->
-            </div><!--/.col-md-12 -->
 
             <div class="col-md-12">
                 @if (Session::has('message'))
@@ -87,13 +40,16 @@
                         <h3 class="card-title">როლის მინიჭება</h3>
                     </div>
                     <div class="mt-4 ml-3 flex flex-d">
-                    @if ($permission->roles)
-                        @foreach ($permission->roles as $permission_role)
-                            <form class="ml-1" method="POST" action="{{route('admin.permissions.roles.remove', [$permission->id, $permission_role->id])}}"
+                    @if ($user->roles)
+                        @foreach ($user->roles as $user_role)
+                            <form class="ml-1"
+                                method="POST"
+                                action="{{route('admin.users.roles.remove', [$user->id, $user_role->id])}}"
                                 onsubmit="return confirm('ნამდვილად გსურთ წაშლა');">
                                 @csrf
                                 @method('DELETE')
-                                <button class="p-2 bg-info text-white ml-2 rounded" type="submit"><i class="nav-icon fas fa-trash"></i> {{$permission_role->name}}</button>
+                                <button class="p-2 bg-info text-white ml-2 rounded" type="submit">
+                                    <i class="nav-icon fas fa-trash"></i> {{$user_role->name}}</button>
                             </form>
                         @endforeach
 
@@ -101,7 +57,7 @@
                     </div>
                     <!-- /.card-header -->
                     <!-- form start -->
-                    <form id="quickForm" method="POST" action="{{route('admin.permissions.role',$permission->id)}}">
+                    <form id="quickForm" method="POST" action="{{route('admin.users.role',$user->id)}}">
                           @csrf
                     <div class="card-body">
                       <div class="form-group">
@@ -121,6 +77,57 @@
                     </form>
                 </div><!-- /.card -->
             </div><!-- /.col-md-12-->
+
+            <!-- start col-md-12 -->
+            <div class="col-md-12">
+                @if (Session::has('message'))
+                <div class="alert alert-success" role="alert">
+                    {{Session::get('message')}}!
+                  </div>
+                @endif
+                <!-- jquery validation -->
+                <div class="card card-primary">
+                  <div class="card-header">
+                    <h3 class="card-title">ნებართვის მინიჭება</h3>
+                  </div>
+                  <div class="mt-4 ml-3 flex flex-d">
+                    @if ($user->permissions)
+                        @foreach ($user->permissions as $user_permission)
+                            <form class="ml-1"
+                                method="POST"
+                                action="{{route('admin.users.permissions.revoke', [$user->id, $user_permission->id])}}"
+                                onsubmit="return confirm('ნამდვილად გსურთ წაშლა');">
+                                @csrf
+                                @method('DELETE')
+                                <button class="p-2 bg-info text-white ml-2 rounded" type="submit"><i class="nav-icon fas fa-trash"></i> {{$user_permission->name}}</button>
+                            </form>
+                        @endforeach
+
+                    @endif
+                </div><!-- /.card-header -->
+
+                    <!-- form start -->
+                    <form id="quickForm"
+                        method="POST"
+                        action="{{route('admin.users.permissions',$user->id)}}">
+                        @csrf
+                    <div class="card-body">
+                      <div class="form-group">
+                        <label for="name">ნებართვა</label>
+                        <select class="form-control" name="permission" id="">
+                            <option value="">აირჩიე ნებართვა</option>
+                            @foreach ($permissions as $permission)
+                            <option value="{{$permission->name}}">{{$permission->name}}</option>
+                            @endforeach
+                        </select>
+                      </div>
+                    </div><!-- /.card-body -->
+                    <div class="card-footer">
+                      <button type="submit" class="btn btn-primary">შენახვა</button>
+                    </div>
+                    </form>
+                </div><!-- /.card -->
+            </div><!--/.col-md-12 -->
 
         </div><!-- /.row -->
     </div><!-- /.container-fluid -->
