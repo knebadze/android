3b38a7f96d44092cc54a485f875d4fe357155449
knebadze <nebo.nebadze98@gmail.com>
2022-09-26 09:41:27 +0400

    first commit

--- app/View/Components/InterviewerLayout.php
+++ app/View/Components/InterviewerLayout.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class InterviewerLayouts extends Component
+class InterviewerLayout extends Component
 {
     /**
      * Get the view / contents that represents the component.
